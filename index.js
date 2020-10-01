3adc73f79dde2822ea4410f1874952f4d6960a02
rjm <rjmatt77@googlemail.com>
2020-10-01 16:38:58 +0100

    basic node setup

--- index.js
+++ index.js
@@ -2,15 +2,11 @@ const express = require('express')
 const app = express()
 const server = require('http').createServer(app)
 const path = require('path')
-const io = require('socket.io')(server)
 
 app.use(
   express.static( path.join(__dirname, '/static') )
 )
 
-io.on('connection', socket => {
-    console.log('Some client connected')
-})
 
 app.get('/', (req, res, next) => {
   res.send('<h1>Hello World</h1>')
